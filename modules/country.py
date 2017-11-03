dcfbe5305effe20383dc6e250331b027170d0a8a
Harini Prabhu <HariniP@gotransit.local>
2017-11-03 12:00:48 -0400

    Modifications in country.py print statements

--- modules/country.py
+++ modules/country.py
@@ -2,7 +2,8 @@ import json
 
 with open('assets/country.json') as json_data:
     d = json.load(json_data)
-
+print("Welcome! You can ask me about location, capital, currency, population, area, population density, GDP, life expectancy, birth rate and death rate of any country \n\nPlease remember to start the country name with a capital letter \n")
+print("Format: {Field} of {Country}")
 keyString = input("Please enter what you need to know in the following format \
 {Field} of {Country} or enter just the {field}\
  to get the summarized country details: ")
