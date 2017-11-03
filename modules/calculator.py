2479482e19d5b4bd35a731acafe1197117a661b6
Harini Prabhu <HariniP@gotransit.local>
2017-11-03 12:52:12 -0400

    Removed redundant print statements in two out of three cases.

--- modules/calculator.py
+++ modules/calculator.py
@@ -17,13 +17,13 @@ def validateOperator(prompt):
     while True:
         value = input(prompt)
         if value not in ('+', '-', '*', '/', '%', '^'):
-            print ("Please enter a valid operator")
+            print ("Please enter a valid operator (+, -, *, /, %, ^)")
         else:
             break
     return value
 
 def askDecision():
-    decision = validateDecision("Do you want to continue the operation?\n Press 1 to continue operation,\
+    decision = validateDecision("\nDo you want to continue the operation?\n Press 1 to continue operation,\
 \n Press 2 to clear the result and start a new calculation \n Press 3 to Exit calculator\n")
     return decision
         
@@ -31,7 +31,7 @@ def validateDecision(prompt):
     while True:
         value = input(prompt)
         if value not in ('1','2','3'):
-            print ("Please enter a valid decision")
+            print ("Please enter a valid decision (1 or 2 or 3)")
         else:
             break
     return value
@@ -46,7 +46,7 @@ def calculate(operator1,op,operator2):
         try:
             result =  (operator1) /  (operator2)
         except ZeroDivisionError:
-            print("The second operator cannot be zero")
+            print("The second input cannot be zero")
             result = repeatSecondInput(operator1,op,operator2)
     elif op == '*':
         result =  (operator1) *  (operator2)
@@ -68,7 +68,7 @@ def continueCalculation(result):
             number3 = validateNumber("Please enter the next number: ")
             #print(number3)
             result1 = calculate(result, operation2, number3)
-            print ("The result of ",result, operation2, number3,"is", result1)
+            #print ("====> The result of ",result, operation2, number3,"is", result1)
             result = result1
             continue
         elif decision == '2':
@@ -76,7 +76,7 @@ def continueCalculation(result):
             operation1 = validateOperator("Please enter an operator you wish to use (+,-,*,/,%,^): ")
             number2 = validateNumber("Please enter the second number: ")
             result = calculate(number1,operation1,number2)
-            print ("The result of ",number1, operation1, number2,"is", result)
+            #print ("====> The result of ",number1, operation1, number2,"is", result)
             continue
         elif decision == '3':
             break
@@ -88,10 +88,10 @@ def repeatSecondInput(number1,operation1,number2):
     return result
 
 def printResult(number1,operation1,number2,result):
-     print ("The result of ",number1, operation1, number2,"is", result)
+     print ("====> The result of ",number1, operation1, number2,"is", result)
 
 def calculator_main():
-    number1 = validateNumber("Please enter the first number: ")
+    number1 = validateNumber("\nPlease enter the first number: ")
     operation1 = validateOperator("Please enter an operator you wish to use (+,-,*,/,%,^): ")
     number2 = validateNumber("Please enter the second number: ")
     result = calculate(number1,operation1,number2)
