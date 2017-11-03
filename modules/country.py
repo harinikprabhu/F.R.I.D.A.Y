a4008fed69ce4176883e5ca659c39776b3e246cf
Harini Prabhu <HariniP@gotransit.local>
2017-11-03 15:01:51 -0400

    KeyError correction

--- modules/country.py
+++ modules/country.py
@@ -2,7 +2,7 @@ import json
 
 with open('assets/country.json') as json_data:
     d = json.load(json_data)
-print("Welcome! You can ask me about location, capital, currency, population, area, population density, GDP, life expectancy, birth rate and death rate of any country \n\nPlease remember to start the country name with a capital letter \n")
+print("Welcome! You can ask me about location, capital, currency, population, area, population density, GDP, life exp, birth rate and death rate of any country \n\nPlease remember to start the country name with a capital letter \n")
 print("Format: {Field} of {Country}")
 keyString = input("Please enter what you need to know in the following format \
 {Field} of {Country} or enter just the {field}\
@@ -11,9 +11,14 @@ keyString = input("Please enter what you need to know in the following format \
 inputList = keyString.split(" of ")
 
 if (len(inputList)==2):
-    information = d[inputList[1]][inputList[0]]
-    print (information)
-elif (len(inputList) == 1 ):
+    try:
+        information = d[inputList[1]][inputList[0]]
+        print (information)
+    except KeyError:
+        print("Enter a valid query")
+        
+
+elif (len(inputList) == 1):
     count = 0
     for a in d:
         if count <= 10:
